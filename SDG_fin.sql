168d5486c1e2876ca2e5ff022992fb1f474aaef7
zhezhemu <142883972+zhezhemu@users.noreply.github.com>
2024-04-23 00:06:10 +1200

    Add files via upload

---
+++ SDG_fin.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 158888
+Source Server Version : 50716
+Source Host           : guojiban1:3306
+Source Database       : SDG_fin
+
+Target Server Type    : MYSQL
+Target Server Version : 50716
+File Encoding         : 65001
+
+Date: 2024-04-23 00:03:11
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
